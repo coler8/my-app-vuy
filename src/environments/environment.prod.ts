84a705293d4bf7f3e20aa1d51e1263b5cd22180f
Samuel <35542193+coler8@users.noreply.github.com>
2023-10-28 11:59:05 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCVyCErzf5mEJBDCCsQMkI0fAV3nTS39p0",
+    apiKey: "",
     authDomain: "vuyapp-proyecto.firebaseapp.com",
     databaseURL: "https://vuyapp-proyecto.firebaseio.com",
     projectId: "vuyapp-proyecto",
     storageBucket: "",
-    messagingSenderId: "821869491165"
+    messagingSenderId: ""
   }
 };
